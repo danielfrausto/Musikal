c3a74b03cc5bcf1bdbf51be4abc6f2b3fd55807a
Daniel Frausto <danielfrausto@live.com>
2019-07-18 15:33:55 -0700

    Minor comment update.

--- Program.cs
+++ Program.cs
@@ -36,6 +36,7 @@ namespace Musikal
             int NoteIndex = ChromaticNotes.IndexOf(ScaleRoot);      //<<    Set scale root index.
             //Console.WriteLine(NoteIndex);
 
+            //  Chromatic notes index cheat sheet.
             //  0  1  2  3  4  5  6  7  8  9  10  11
             //  C  C# D  D# E  F  F# G  G# A  A#  B 
             int nextIndex = NoteIndex;
