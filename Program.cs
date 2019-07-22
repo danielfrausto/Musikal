4953efd97cf4c770f2f3333b77cb38189bed9da7
Daniel Frausto <danielfrausto@live.com>
2019-07-21 19:02:05 -0700

    ScaleDictionaries Added.

--- Program.cs
+++ Program.cs
@@ -12,18 +12,39 @@ namespace Musikal
         static List<string> ChromaticNotes = new List<string>();
         //  HS = 1
         //  FS = 2
-        static  int[] Major = {0,2,2,1,2,2,2,1 };
+        //static int[] Major = { 0, 2, 2, 1, 2, 2, 2, 1 };
+        //static int[] Dorian = { 0, 2, 1, 2, 2, 2, 1, 2 };
 
+        //  static Dictionary<string, dynamic> {{ScaleName}} = new Dictionary<string, dynamic>() { { "Name","{{ScaleName}}"},{ "Steps", new int[] { {{ScaleSteps}} } } };
+
+        static Dictionary<string, dynamic> Major = new Dictionary<string, dynamic>() { { "Name","Major"},{ "Definition", new int[] { 0, 2, 2, 1, 2, 2, 2, 1 } } };
         static void Main(string[] args)
         {
             SetChromaticNotes();                                    //<<    Set chromatic notes. Required!!!
 
-            string ScaleRoot = "F";                                 //<<    Set scale root.
-            int[] ScaleDefinition = Major;                          //<<    Set scale definition to use, e.g, Major or Minor. 
-            pScale(ScaleRoot, ScaleDefinition);
-            
+            //string ScaleRoot = "F";                                 //<<    Set scale root.
+            //int[] ScaleDefinition = Major;                          //<<    Set scale definition to use, e.g, Major or Minor. 
+            //pScale(ScaleRoot, ScaleDefinition);
+
+            //pAll();
+            dicTest(Major);
             Console.ReadKey();                                      //<<    Keep app alive.
         }
+        static void dicTest(Dictionary<string, dynamic> ThisDict)
+        {
+            Console.WriteLine(ThisDict["Name"]);
+            foreach(int Step in ThisDict["Definition"])
+            {
+                Console.WriteLine(Step);
+            }
+        }
+        //static void pAll()
+        //{
+        //    foreach(string ScaleRoot in ChromaticNotes)
+        //    {
+        //        pScale(ScaleRoot, Dorian);
+        //    }
+        //}
         static void SetChromaticNotes()
         {
             foreach (char charNote in MusicNotes)
@@ -42,13 +63,15 @@ namespace Musikal
         }
         static void pScale(string ScaleRoot, int[]ScaleDefinition)
         {
+            Console.WriteLine(ScaleRoot + " " + ScaleDefinition.GetType().Name);
+            //  Chromatic notes index cheat sheet.
+            //  0  1  2  3  4  5  6  7  8  9  10  11
+            //  C  C# D  D# E  F  F# G  G# A  A#  B 
+
             List<string> ScaleNotes = new List<string>();
             int NoteIndex = ChromaticNotes.IndexOf(ScaleRoot);      //<<    Set scale root index.
             //Console.WriteLine(NoteIndex);
 
-            //  Chromatic notes index cheat sheet.
-            //  0  1  2  3  4  5  6  7  8  9  10  11
-            //  C  C# D  D# E  F  F# G  G# A  A#  B 
             int nextIndex = NoteIndex;
             foreach (int step in ScaleDefinition)
             {
